132a49f32835674d94a42cf959f2abec261fb549
yinyong02 <yinyong02@baidu.com>
2015-04-24 16:47:21 +0800

    Main js file

---
+++ router.js
@@ -0,0 +1,12 @@
+/**
+  * Copyright (C) 2015 yanni4night.com
+  * router.js
+  *
+  * changelog
+  * 2015-04-24[16:31:20]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+ 
